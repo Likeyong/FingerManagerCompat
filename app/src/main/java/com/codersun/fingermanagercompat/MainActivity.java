732d70b10900222b1479f36aafd256509dc24886
K1081308 <k1081308@test.htsc.com.cn>
2020-08-19 19:08:44 +0800

    demo添加同步指纹数据功能

--- app/src/main/java/com/codersun/fingermanagercompat/MainActivity.java
+++ app/src/main/java/com/codersun/fingermanagercompat/MainActivity.java
@@ -63,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+
+            findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FingerManager.updateFingerData(MainActivity.this);
+                }
+            });
+
         }
 
     }
